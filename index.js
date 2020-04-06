2d80ec08f574e126cf06269e4e5fc231849fe6a2
Elliot Plant <elliotaplant@gmail.com>
2020-04-06 12:42:53 -0500

    Get it all happy

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 window.city.onkeyup = () => {
   if (window.city.value) {
     window.matchingCitiesList.innerHTML = Object.keys(window.cities)
-      .filter(city => city.toLowerCase().startsWith(window.city.value.toLowerCase()))
-      .map(city => `<li style="color: ${window.cities[city]}">
-        ${city}
-        <button type="button" data-city=${city} onclick=drawInfectionCard(event)>Infect</button>
-        <button type="button" data-city=${city} onclick=epidemic(event)>Epidemic</button>
-      </li>`)
+      .filter(city => city.toLowerCase().startsWith(window.city.value.toLowerCase()) && !resiliantPopulations.includes(city))
+      .map(city => {
+        let buttons = '';
+        if (deckTiers[deckTiers.length - 1].includes(city)) {
+          buttons += `<button type="button" data-city="${city}" onclick=drawInfectionCard(event)>Infect</button>`;
+        }
+        if (deckTiers[0].includes(city)) {
+          buttons += `<button type="button" data-city="${city}" onclick=epidemic(event)>Epidemic</button>`;
+        }
+        if (infectionDiscardPile.includes(city) && !resiliantPopulations.includes(city)) {
+          buttons = `<button type="button" data-city="${city}" onclick=resiliant(event)>Resiliant Population</button>`;
+        }
+        return `<li style="color: ${window.cities[city]}"> ${city} ${buttons} </li>`;
+      })
       .join('\n');
   } else {
     window.matchingCitiesList.innerHTML = '';
--- state.js
+++ state.js
@@ -1,14 +1,20 @@
+const uniq = (arr) => Array.from(new Set(arr));
 const last = (arr) => arr[arr.length - 1];
 const setLast = (arr, val) => {
   arr[arr.length - 1] = val;
 };
 
-const deckTiers = JSON.parse(localStorage.getItem('deckTiers')) || [Object.keys(window.cities)];
+const numProbabilities = [1,2,3,4];
+
+let deckTiers = JSON.parse(localStorage.getItem('deckTiers')) || [Object.keys(window.cities)];
 let infectionDiscardPile = JSON.parse(localStorage.getItem('infectionDiscardPile')) || [];
+const resiliantPopulations = JSON.parse(localStorage.getItem('resiliantPopulations')) || [];
+let probabilityTiers = [];
 
 function epidemic(event) {
   const bottomCard = event.target.dataset.city;
   infectionDiscardPile.push(bottomCard);
+  infectionDiscardPile = uniq(infectionDiscardPile);
   deckTiers[0] = deckTiers[0].filter(k => k !== bottomCard);
   deckTiers.push(infectionDiscardPile);
   infectionDiscardPile = [];
@@ -16,6 +22,7 @@ function epidemic(event) {
 
 function drawInfectionCard(event) {
   const city = event.target.dataset.city;
+  console.log('city', city);
   setLast(deckTiers, last(deckTiers).filter(c => c !== city));
   infectionDiscardPile.push(city);
   if (last(deckTiers).length === 0) {
@@ -23,12 +30,17 @@ function drawInfectionCard(event) {
   }
 }
 
+function resiliant(event) {
+  const city = event.target.dataset.city;
+  infectionDiscardPile = infectionDiscardPile.filter(c => c !== city);
+  resiliantPopulations.push(city);
+}
+
 function uiUpdater(fn) {
   return (...args) => {
     fn(...args);
+    updateProbabilities();
     updateUI();
-    localStorage.setItem('deckTiers', JSON.stringify(deckTiers));
-    localStorage.setItem('infectionDiscardPile', JSON.stringify(infectionDiscardPile));
   };
 }
 
@@ -36,19 +48,78 @@ function sortByColor(cityList) {
   return cityList.sort((a, b) => window.cities[a] < window.cities[b] ? 1 : -1);
 }
 
+function updateProbabilities() {
+  probabilityTiers = deckTiers.slice().reverse().map(tier => tier.length).reduce(({ out, cardsAbove } , tierSize) => {
+    out.push(numProbabilities.map(cardsDrawn => {
+      let majungus = 1;
+      for (let i = 1; i <= cardsDrawn - cardsAbove; i++) {
+        majungus *= (tierSize - i)/Math.max(tierSize - (i - 1), 1);
+      }
+      return 1 - majungus;
+    }));
+    cardsAbove = cardsAbove + tierSize;
+    return { out, cardsAbove };
+  }, { out: [], cardsAbove: 0 }).out;
+}
+
 function updateUI() {
-  window.tiers.innerHTML = deckTiers.slice().reverse().map(tier => {
+  window.tiers.innerHTML = deckTiers.slice().reverse().map((tier, i) => {
     const tierList = sortByColor(tier)
       .map(city => `<li class="tier-city" style="color: ${window.cities[city]}">${city}</li>`)
       .join('\n');
-    return `<ul class="tier city-list">${tierList}</ul>`;
+    const probs = probabilityTiers[i].map(prob => Math.round(prob * 100) + '%');
+    return `<div>
+      <div>${probs.map(p => `<span class="prob">${p}</span>`).join(' ')}</div>
+      <ul class="tier city-list">${tierList}</ul>
+    </div>`;
   }).join('\n');
 
   window.discardPile.innerHTML = sortByColor(infectionDiscardPile)
+    .map(city => `<li class="tier-city" style="color: ${window.cities[city]}"><span>${city}</span></li>`)
+    .join('\n');
+
+  window.resiliantPopulations.innerHTML = sortByColor(resiliantPopulations)
     .map(city => `<li class="tier-city" style="color: ${window.cities[city]}">${city}</li>`)
     .join('\n');
+
+  if (window.city.value) {
+    window.matchingCitiesList.innerHTML = Object.keys(window.cities)
+      .filter(city => city.toLowerCase().startsWith(window.city.value.toLowerCase()) &&
+      !resiliantPopulations.includes(city))
+      .map(city => {
+        let buttons = '';
+        if (deckTiers[deckTiers.length - 1].includes(city)) {
+          buttons += `<button type="button" data-city="${city}" onclick=drawInfectionCard(event)>Infect</button>`;
+        }
+        if (deckTiers[0].includes(city)) {
+          buttons += `<button type="button" data-city="${city}" onclick=epidemic(event)>Epidemic</button>`;
+        }
+        if (infectionDiscardPile.includes(city) && !resiliantPopulations.includes(city)) {
+          buttons = `<button type="button" data-city="${city}" onclick=resiliant(event)>Resiliant Population</button>`;
+        }
+        return `<li style="color: ${window.cities[city]}"> ${city} ${buttons} </li>`;
+      })
+      .join('\n');
+  } else {
+    window.matchingCitiesList.innerHTML = '';
+  }
+
+  localStorage.setItem('deckTiers', JSON.stringify(deckTiers));
+  localStorage.setItem('infectionDiscardPile', JSON.stringify(infectionDiscardPile));
 }
+
+
+
+window.reset = () => {
+  deckTiers = [Object.keys(window.cities)];
+  infectionDiscardPile = [];
+  updateProbabilities();
+  updateUI();
+};
 
 window.epidemic = uiUpdater(epidemic);
 window.drawInfectionCard = uiUpdater(drawInfectionCard);
+window.resiliant = uiUpdater(resiliant);
+window.city.onkeyup = uiUpdater(() => {});
+updateProbabilities();
 updateUI();
